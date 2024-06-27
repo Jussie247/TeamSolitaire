d5795206e3e1ceea74ff279b662cfa83605101df
Jussie247 <justus.schultzke@gmail.com>
2024-06-27 11:24:07 +0200

    connect4Beginning

--- TeamSolitaire/Projects/connect4/connect4.js
+++ TeamSolitaire/Projects/connect4/connect4.js
@@ -1,1 +1,12 @@
 "use strict";
+var connect4;
+(function (connect4) {
+    let rows;
+    let cols;
+    let currentPlayer = "player1";
+    let board;
+    window.addEventListener("load", handleLoad);
+    function handleLoad() {
+        const startButton = document.getElementById("start-game");
+    }
+})(connect4 || (connect4 = {}));
---
+++ TeamSolitaire/Projects/connect4/connect4.ts
@@ -0,0 +1,14 @@
+namespace connect4 {
+    let rows: number;
+    let cols: number;
+    let currentPlayer: string = "player1";
+    let board: HTMLElement;
+
+    window.addEventListener("load", handleLoad);
+
+    function handleLoad(): void {
+        const startButton = document.getElementById("start-game")!;
+        
+    }
+
+}
